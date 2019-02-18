e1a9068ba41ff448165f3db42c2f533faaf10470
Rafael <tschese.smurf@gmail.com>
2019-02-18 16:44:10 +0100

    database error fixed and bootstrap added

--- authentication/login.php
+++ authentication/login.php
@@ -58,6 +58,7 @@ if (isset($_POST['password']) && isset($_POST['password']) && $_POST['password']
 <html>
 <head>
   <title>SoundHub - Login</title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 <body>
   <section class="hero">
