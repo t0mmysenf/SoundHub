4f04d5831ab0e950cbe52a39b013728fcae973ed
jwohlhauser <38973875+jwohlhauser@users.noreply.github.com>
2019-02-18 15:29:14 +0100

    Update index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>SoundHub</title>
+</head>
+<body>
+
+  	<center>
+      <h2>This website is a place to share and listen to your favourite music.</h2>
+      <a href="authentication/register.php">
+        <button class="button" style="vertical-align:middle"><span>Sign up </span></button>
+      </a>
+      <a href="authentication/login.php">
+        <button class="button" style="vertical-align:middle"><span>Log in </span></button>
+      </a>
+    </center>
+
+</body>
+</html>
